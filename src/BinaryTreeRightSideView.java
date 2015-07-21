698e15f261ca8d55a06ab31561c22b6c9a263614
Thanakorn Trakarnvanich <thanakorn@nyu.edu>
2015-07-20 22:36:33 -0400

    update

---
+++ src/BinaryTreeRightSideView.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Thanakorn on 7/20/15.
+ */
+public class BinaryTreeRightSideView {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> rightSideView(TreeNode root) {
+        List<Integer> rsv = new ArrayList<>();
+        if (root == null) {
+            return rsv;
+        }
+
+        List<TreeNode> curLevel = new ArrayList<>();
+        curLevel.add(root);
+
+        while (!curLevel.isEmpty()) {
+            List<TreeNode> nextLevel = new ArrayList<>();
+            for (int i = 0; i < curLevel.size(); i++) {
+                TreeNode curNode = curLevel.get(i);
+                if (i == curLevel.size() - 1) {
+                    rsv.add(curNode.val);
+                }
+                if (curNode.left != null) {
+                    nextLevel.add(curNode.left);
+                }
+                if (curNode.right != null) {
+                    nextLevel.add(curNode.right);
+                }
+            }
+            curLevel = nextLevel;
+        }
+
+        return rsv;
+    }
+
+}
--- src/MyAtoi.java
+++ src/MyAtoi.java
@@ -9,6 +9,10 @@ public class MyAtoi {
 
     public static int myAtoi(String str) {
 
+        if (str.isEmpty()) {
+            return 0;
+        }
+
         int startIndex = -1;
         for (int i = 0; i < str.length(); i++) {
             char c = str.charAt(i);
@@ -18,12 +22,39 @@ public class MyAtoi {
             }
         }
 
+        int mul = 1;
         if (str.charAt(startIndex) == '+') {
-
+            startIndex++;
+        } else if (str.charAt(startIndex) == '-') {
+            mul = -1;
+            startIndex++;
         }
-        if (str.charAt(startIndex) == '-') {
 
+        if (startIndex >= str.length()) {
+            return 0;
         }
-        return 0;
+        if (!str.substring(startIndex, startIndex+1).matches("[0-9]")) {
+            return 0;
+        }
+
+        int endIndex = str.length();
+        for (int i = startIndex+1; i < str.length(); i++) {
+            String curChar = str.substring(i,i+1);
+            if (!curChar.matches("[0-9]")) {
+                endIndex = i;
+                break;
+            }
+        }
+
+        try {
+            int num = Integer.parseInt(str.substring(startIndex, endIndex));
+            return num * mul;
+        } catch (NumberFormatException e) {
+            if (mul == 1) {
+                return Integer.MAX_VALUE;
+            } else {
+                return Integer.MIN_VALUE;
+            }
+        }
     }
 }
