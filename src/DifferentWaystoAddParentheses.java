2985e8debe1dce1f84d4ee3586a8f2248f1b493a
Thanakorn Trakarnvanich <thanakorn@nyu.edu>
2015-07-28 20:59:05 -0400

    update

---
+++ src/DifferentWaystoAddParentheses.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+/**
+ * Created by Thanakorn on 7/28/15.
+ */
+
+class PNode {
+    
+
+}
+
+public class DifferentWaystoAddParentheses {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static List<Integer> diffWaysToCompute(String input) {
+
+
+        return null;
+    }
+}
---
+++ src/SevenHeadsOrTailsInARow.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Thanakorn on 7/27/15.
+ */
+public class SevenHeadsOrTailsInARow {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double getWinningProbability(int n, int k) {
+
+
+        return 0;
+    }
+}
