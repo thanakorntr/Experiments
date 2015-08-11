064ca0692568f175d2b7388d9fbf225dade84367
Thanakorn Trakarnvanich <thanakorn@nyu.edu>
2015-08-10 20:34:33 -0400

    update

--- src/Test.java
+++ src/Test.java
@@ -12,7 +12,7 @@ public class Test {
     private Stack<Integer> curStack = s1;
 
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("2");
     }
 
 
