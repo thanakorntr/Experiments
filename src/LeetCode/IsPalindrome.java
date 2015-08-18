19cc2ea623360d1c5619789dd2fbc808edec1094
Thanakorn Trakarnvanich <thanakorn@nyu.edu>
2015-08-17 21:47:14 -0400

    update

---
+++ src/LeetCode/IsPalindrome.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+/**
+ * Created by Thanakorn on 8/17/15.
+ */
+public class IsPalindrome {
+
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+
+        int palindrome = 0;
+        int tmp = x;
+
+        while (tmp > 0) {
+            int lastDigit = tmp % 10;
+            palindrome = palindrome*10 + lastDigit;
+            tmp = tmp / 10;
+        }
+        
+        return palindrome == x;
+    }
+
+}
