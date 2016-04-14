5663dfe5db9e9d7e3e72d255836bdd379f69dcec
Thanakorn Trakarnvanich <thanakorn@nyu.edu>
2016-04-13 20:19:16 -0400

    update

--- src/LeetCode/PalindromePartitioning.java
+++ src/LeetCode/PalindromePartitioning.java
@@ -3,6 +3,18 @@ package LeetCode;
 import java.util.*;
 
 /**
+ * Given a string s, partition s such that every substring of the partition is a palindrome.
+
+ Return all possible palindrome partitioning of s.
+
+ For example, given s = "aab",
+ Return
+
+ [
+ ["aa","b"],
+ ["a","a","b"]
+ ]
+ *
  * Created by Thanakorn on 7/13/15.
  */
 public class PalindromePartitioning {
