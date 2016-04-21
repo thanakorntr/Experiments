1fc34d416e2e1d9a5af27035a46e526736e739c7
Thanakorn Trakarnvanich <thanakorn@nyu.edu>
2016-04-21 19:27:57 -0400

    update

---
+++ src/LeetCodeII/SearchinRotatedSortedArray.java
@@ -0,0 +1,47 @@
+package LeetCodeII;
+
+/**
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+ (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+ You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+ You may assume no duplicate exists in the array.
+ *
+ * Created by Thanakorn on 4/21/16.
+ */
+public class SearchInRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int left = 0, right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > nums[left]) {  // left side is in order
+                if (nums[mid] > target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else if (nums[mid] < nums[right]){  // right side is in order
+                if (nums[mid] < target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+
+}
