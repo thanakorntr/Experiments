bcdc9b0b747ed4f0f0fb043b413b2a62d4e5dcd6
Thanakorn Trakarnvanich <thanakorn@nyu.edu>
2016-07-11 22:10:55 -0400

    update

--- src/LeetCodeII/DistinctSubsequences.java
+++ src/LeetCodeII/DistinctSubsequences.java
@@ -1,7 +1,9 @@
-package LeetCode;
+package LeetCodeII;
+
+import java.util.*;
 
 /**
- * Given a string S and a string T, count the number of distinct subsequences of T in S.
+ *  * Given a string S and a string T, count the number of distinct subsequences of T in S.
 
  A subsequence of a string is a new string which is formed from the original string by deleting some (can be none) of the characters without disturbing the relative positions of the remaining characters. (ie, "ACE" is a subsequence of "ABCDE" while "AEC" is not).
 
@@ -10,7 +12,7 @@ package LeetCode;
 
  Return 3.
  *
- * Created by Thanakorn on 5/27/16.
+ * Created by Thanakorn on 7/11/16.
  */
 public class DistinctSubsequences {
 
@@ -29,58 +31,45 @@ public class DistinctSubsequences {
         if (s == null || t == null) {
             return 0;
         }
-
-        if (s.isEmpty() || t.isEmpty()) {
-            if (s.isEmpty()) {
-                return 0;
-            } else if (!s.isEmpty()) {
-                return s.length();
-            }
-        }
-
-        int[][] memo = new int[s.length()][t.length()];
-        for (int row = 0; row < s.length(); row++) {
-            for (int col = 0; col < t.length(); col++) {
-                memo[row][col] = -1;
-            }
-        }
-
+        Map<Integer, Map<Integer, Integer>> memo = new HashMap<>();
         return numDistinctHelper(s, t, 0, 0, memo);
     }
 
     private int numDistinctHelper(String s, String t,
-                                  int startS, int startT,
-                                  int[][] memo) {
+                                   int sIndex, int tIndex,
+                                   Map<Integer, Map<Integer, Integer>> memo) {
 
-        if (memo[startS][startT] != -1) {
-            return memo[startS][startT];
+        if (s.length() - sIndex < t.length() - tIndex) {
+            return 0;
+        }
+
+        if (!memo.containsKey(sIndex)) {
+            memo.put(sIndex, new HashMap<>());
+        }
+        if (sIndex >= s.length() || tIndex >= t.length()) {
+            int count;
+            if (sIndex >= s.length() && tIndex >= t.length()) {
+                count = 1;
+            } else if (sIndex >= s.length()) {
+                count = 0;
+            } else {
+                count = 1;
+            }
+            memo.get(sIndex).put(tIndex, count);
+            return count;
+        }
+
+        if (memo.containsKey(sIndex) && memo.get(sIndex).containsKey(tIndex)) {
+            return memo.get(sIndex).get(tIndex);
         }
 
         int count = 0;
-        for (int i = startS; i < s.length(); i++) {
-            if (s.length() - i < t.length() - startT) {
-                break;
-            }
-            if (s.charAt(i) == t.charAt(startT)) {
-                int nextStartS = i + 1;
-                int nextStartT = startT + 1;
-                if (nextStartS >= s.length() || nextStartT >= t.length()) {
-                    if (nextStartS >= s.length() && nextStartT >= t.length()) {
-                        count++;
-                    } else if (nextStartT >= t.length()) {
-                        count++;
-                    }
-                    continue;
-                } else {
-                    if (memo[nextStartS][nextStartT] == -1) {
-                        memo[nextStartS][nextStartT] = numDistinctHelper(s, t, nextStartS, nextStartT, memo);
-                    }
-                }
-                count += memo[nextStartS][nextStartT];
-            }
+        if (s.charAt(sIndex) == t.charAt(tIndex)) {
+            count += numDistinctHelper(s, t, sIndex + 1, tIndex + 1, memo);
         }
+        count += numDistinctHelper(s, t, sIndex + 1, tIndex, memo);
 
-        memo[startS][startT] = count;
+        memo.get(sIndex).put(tIndex, count);
         return count;
     }
 
